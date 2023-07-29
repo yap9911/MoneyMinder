da101c1e3137e2c050d09e04858a253d7dab3162
yap9911 <75289918+yap9911@users.noreply.github.com>
2023-07-29 15:51:48 +0800

    mobile tracking application

--- app/src/main/java/my/edu/utar/moneyminder/MainActivity.java
+++ app/src/main/java/my/edu/utar/moneyminder/MainActivity.java
@@ -21,6 +21,7 @@ public class MainActivity extends AppCompatActivity {
 
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+        //test
 
         BottomNavigationView navView = findViewById(R.id.nav_view);
         // Passing each menu ID as a set of Ids because each
