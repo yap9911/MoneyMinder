35c89e074bc93efa214c96eb4157b6450b854108
yap9911 <75289918+yap9911@users.noreply.github.com>
2023-08-17 01:20:47 +0800

    added addFAB and removeFAB

--- app/src/main/java/my/edu/utar/moneyminder/ui/home/HomeFragment.java
+++ app/src/main/java/my/edu/utar/moneyminder/ui/home/HomeFragment.java
@@ -1,13 +1,15 @@
 package my.edu.utar.moneyminder.ui.home;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
 
+import my.edu.utar.moneyminder.MainActivity;
 import my.edu.utar.moneyminder.databinding.FragmentHomeBinding;
 
 public class HomeFragment extends Fragment {
@@ -20,10 +22,35 @@ public class HomeFragment extends Fragment {
         binding = FragmentHomeBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        final TextView textView = binding.textHome;
-        textView.setText("This is home fragment");
+        FloatingActionButton addFAB = binding.addFAB;
+        FloatingActionButton removeFAB = binding.removeFAB;
+
+        addFAB.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        removeFAB.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
         return root;
     }
+
+//    public void openCashInActivity() {
+//        Intent intent = new Intent(getContext(), CashInActivity.class);
+//        startActivity(intent);
+//    }
+//
+//    public void openCashOutActivity() {
+//        Intent intent = new Intent(getContext(), CashOutActivity.class);
+//        startActivity(intent);
+//    }
 
     @Override
     public void onDestroyView() {
